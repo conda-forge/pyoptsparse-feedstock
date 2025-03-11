c8fc4010d6fa56fd9f28c2d41f99815950d65c4b
Ella Wu <602725+ewu63@users.noreply.github.com>
2025-03-10 21:09:32 -0700

    no need to set env var on windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,6 @@
 @echo on
 
 cd %SRC_DIR%
-set CC=cl
-set FC=flang
-set CC_LD=link
 
 REM See the unix build.sh for more details on the build process below.
 set MESON_ARGS=-Dipopt_dir=%LIBRARY_PREFIX% -Dpython_target=%PYTHON% %EXTRA_FLAGS%
