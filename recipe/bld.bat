f289b36e9ce98e1267bd245315f053aafbac22b4
Phil Chiu <phil.chiu@archer.com>
2025-03-12 15:40:37 -0600

    print env vars in windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,9 @@
 
 cd %SRC_DIR%
 
+REM DEBUG - print env vars
+set
+
 REM See the unix build.sh for more details on the build process below.
 set MESON_ARGS=-Dipopt_dir=%LIBRARY_PREFIX% -Dpython_target=%PYTHON% %EXTRA_FLAGS%
 python -m build -n -x .
