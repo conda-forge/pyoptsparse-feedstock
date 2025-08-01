1ded16c011db1e6a9b398ef5f596cf9aa83df8f9
Ella Wu <602725+ewu63@users.noreply.github.com>
2025-08-01 13:30:01 -0700

    forgot Windows build script lives separately

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,6 @@
 cd %SRC_DIR%
 
 REM See the unix build.sh for more details on the build process below.
-set MESON_ARGS=-Dipopt_dir=%LIBRARY_PREFIX% -Dpython_target=%PYTHON% %EXTRA_FLAGS%
+set MESON_ARGS=-Dpython_target=%PYTHON% %EXTRA_FLAGS%
 python -m build -n -x .
 pip install --prefix "%PREFIX%" --no-deps --no-index --find-links dist pyoptsparse
