c64cb79c3faee00a856602438c150af55a0d6418
Ella Wu <602725+ewu63@users.noreply.github.com>
2026-02-06 00:51:04 -0800

    try just invoking pip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,4 @@
 
 cd %SRC_DIR%
 
-REM See the unix build.sh for more details on the build process below.
-set MESON_ARGS=-Dpython_target=%PYTHON% %EXTRA_FLAGS%
-python -m build -n -x .
-pip install --prefix "%PREFIX%" --no-deps --no-index --find-links dist pyoptsparse
+pip install .
