6b3da2bbd598ec9d53c5db6dcc204e38efb78b13
Ella Wu <602725+ewu63@users.noreply.github.com>
2026-02-08 23:03:18 -0800

    set cross-file correctly

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -eox pipefail
 
 if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
   # Use Meson cross-file flag to enable cross compilation
-  EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt"
+  export MESON_ARGS="$MESON_ARGS --cross-file=$BUILD_PREFIX/meson_cross_file.txt"
 fi
 
 cd "${SRC_DIR}"
