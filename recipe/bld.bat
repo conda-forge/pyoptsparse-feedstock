abbe6193b1053e89dfd5bdd58a4b7a0a5f7a5a4e
Ella Wu <602725+ewu63@users.noreply.github.com>
2026-02-08 22:42:18 -0800

    bring back cross compilation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,4 +2,7 @@
 
 cd %SRC_DIR%
 
-pip install .
+REM See the unix build.sh for more details on the build process below.
+
+%PYTHON% -m build -n -x .
+pip install --prefix "%PREFIX%" --no-deps --no-index --find-links dist pyoptsparse
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,12 @@
 
 set -eox pipefail
 
-pip install .
+if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
+  # Use Meson cross-file flag to enable cross compilation
+  EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt"
+fi
+
+cd "${SRC_DIR}"
+
+$PYTHON -m build -n -x .
+pip install --prefix "${PREFIX}" --no-deps --no-index --find-links dist pyoptsparse
