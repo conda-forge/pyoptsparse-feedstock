2ad3b816e559fd9ea2ca8092647465ab3c793924
Ella Wu <602725+ewu63@users.noreply.github.com>
2026-02-11 23:41:07 -0800

    pass in setup-args to meson-python

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,9 @@ set -eox pipefail
 
 if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
   # Use Meson cross-file flag to enable cross compilation
-  export MESON_ARGS="$MESON_ARGS --cross-file=$BUILD_PREFIX/meson_cross_file.txt"
+  setup_args="--cross-file=$BUILD_PREFIX/meson_cross_file.txt"
 fi
 
 cd "${SRC_DIR}"
 
-$PYTHON -m pip install .
+$PYTHON -m pip install . -Csetup-args="$setup_args"
